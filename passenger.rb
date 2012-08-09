d5a88ed138a7cefde274069922fa6d336165efa6
Steve Randy Tantra <steve.randy@gmail.com>
2012-08-09 23:56:35 +0800

    Update passenger recipe

--- passenger.rb
+++ passenger.rb
@@ -1,3 +1,9 @@
+namespace :deploy do
+  task :restart do
+    passenger.restart
+  end
+end
+
 namespace :passenger do
   desc "Restart Passenger process by creating restart.txt file"
   task :restart do
