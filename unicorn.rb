ee61715fd74314221a685f2e2ee7533ab533ec6a
Steve Randy Tantra <steve.randy@gmail.com>
2012-08-10 11:14:42 +0800

    Add path

--- unicorn.rb
+++ unicorn.rb
@@ -1,5 +1,5 @@
-set_default(:unicorn_config) { "#{current_path}/config/unicorn.rb" }
-set_default(:unicorn_pid) { "#{current_path}/tmp/pids/unicorn.pid" }
+set_default(:unicorn_config_path) { "#{current_path}/config/unicorn.rb" }
+set_default(:unicorn_pid_path) { "#{current_path}/tmp/pids/unicorn.pid" }
 set_default(:unicorn_socket) { "#{shared_path}/sockets/unicorn.sock" }
 
 namespace :deploy do
@@ -11,13 +11,13 @@ end
 namespace :unicorn do
   desc "Start Unicorn process as a daemon"
   task :start do
-    run "cd #{current_path} && BUNDLE_GEMFILE=#{current_path}/Gemfile bundle exec unicorn -c #{unicorn_config} -E production -D"
+    run "cd #{current_path} && BUNDLE_GEMFILE=#{current_path}/Gemfile bundle exec unicorn -c #{unicorn_config_path} -E production -D"
   end
 
   desc "Stop Unicorn process gracefully by sending QUIT signal. All pending requests will be completed before workers are killed."
   task :stop do
-    if remote_file_exists? unicorn_pid
-      run "kill -s QUIT `cat #{unicorn_pid}`"
+    if remote_file_exists? unicorn_pid_path
+      run "kill -s QUIT `cat #{unicorn_pid_path}`"
     else
       puts "Cannot stop Unicorn because pid file does not exist."
     end
@@ -26,8 +26,8 @@ namespace :unicorn do
   desc "Stop Unicorn process immediately by sending TERM signal. All pending request will be dropped immediately."
   task :force_stop do
 
-    if remote_file_exists? unicorn_pid
-      run "kill -s TERM `cat #{unicorn_pid}`"
+    if remote_file_exists? unicorn_pid_path
+      run "kill -s TERM `cat #{unicorn_pid_path}`"
     else
       puts "Cannot stop Unicorn because pid file does not exist."
     end
@@ -41,8 +41,8 @@ namespace :unicorn do
 
   desc "Reload Unicorn process by sending USR2 signal, without droppping any pending requests. If no Unicorn process present, one will be started automatically."
   task :reload do
-    if remote_file_exists? unicorn_pid
-      run "kill -s USR2 `cat #{unicorn_pid}`"
+    if remote_file_exists? unicorn_pid_path
+      run "kill -s USR2 `cat #{unicorn_pid_path}`"
     else
       start
     end
