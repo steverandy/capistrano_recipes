e99424999d07df4ba9ed97205a2088849a650791
Steve Randy Tantra <steve.randy@gmail.com>
2013-02-07 14:27:59 +0800

    Don't modify restart task

--- passenger.rb
+++ passenger.rb
@@ -1,9 +1,3 @@
-namespace :deploy do
-  task :restart do
-    passenger.restart
-  end
-end
-
 namespace :passenger do
   desc "Restart Passenger process by creating restart.txt file"
   task :restart do
