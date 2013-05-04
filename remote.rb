4a84901f079fb4e32787829ee60cd9d1a9d5a9de
Steve Randy Tantra <steve.randy@gmail.com>
2013-05-04 14:32:26 +0800

    Simpler server selection

--- remote.rb
+++ remote.rb
@@ -1,15 +1,15 @@
 namespace :remote do
-  desc "Tail log" 
-  task :log, :roles => :app, :only => {:primary => true} do
+  desc "Tail log"
+  task :log, :roles => :app do
     run "tail -f #{shared_path}/log/#{rails_env}.log" do |channel, stream, data|
       puts  # for an extra line break before the host name
-      puts "#{channel[:host]}: #{data}" 
+      puts "#{channel[:host]}: #{data}"
       break if stream == :err
     end
   end
 
   desc "Start remote console"
-  task :console, :roles => :app, :only => {:primary => true} do
+  task :console, :roles => :app do
     input = ""
     run "cd #{current_path} && bundle exec rails console #{rails_env}" do |channel, stream, data|
       next if data.chomp == input.chomp || data.chomp == ""
